b9c1e0f5148b4d7429b0e14a24277551eb2a3c4c
LifelesBow737 <diegolm115935@outlook.com>
2025-10-31 22:29:31 -0600

    Add Member repository

--- API/Interfaces/IMembersRepository.cs
+++ API/Interfaces/IMembersRepository.cs
@@ -1,13 +1,37 @@
 using System;
 using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
-namespace API.Interfaces;
+namespace API.Data;
 
-public interface IMembersRepository
+public class MembersRepository(AppDbContext context) : IMembersRepository
 {
-    void Update(Member member);
-    Task<bool> SaveAllAsync();
-    Task<IReadOnlyList<Member>> GetMembersAsync();
-    Task<Member?> GetMemberAsync(string id);
-    Task<IReadOnlyList<Photo>> GetPhotosAsync(string memberId);
+    public async Task<Member?> GetMemberAsync(string id)
+    {
+        return await context.Members.FindAsync(id);
+    }
+
+    public async Task<IReadOnlyList<Member>> GetMembersAsync()
+    {
+        return await context.Members.ToListAsync();
+    }
+
+    public async Task<IReadOnlyList<Photo>> GetPhotosAsync(string memberId)
+    {
+        return await context.Members
+            .Where(m => m.Id == memberId)
+            .SelectMany(m => m.Photos)
+            .ToListAsync();
+    }
+
+    public async Task<bool> SaveAllAsync()
+    {
+        return await context.SaveChangesAsync() > 0;
+    }
+
+    public void Update(Member member)
+    {
+        context.Entry(member).State = EntityState.Modified;
+    }
 }
