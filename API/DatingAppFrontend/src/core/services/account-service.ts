617713583bfe437ac310dbf593c6f5f4b6393437
LifelesBow737 <diegolm115935@outlook.com>
2025-12-10 19:39:41 -0600

    Parcial03 -Add Member to interface to SPA.

--- API/DatingAppFrontend/src/core/services/account-service.ts
+++ API/DatingAppFrontend/src/core/services/account-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { LoginCreds, User } from '../../types/user';
+import { LoginCreds, RegisteredCreds, User } from '../../types/user';
 import { Observable, tap } from 'rxjs';
 import { RegisterCreds } from '../../types/registerCreds';
 @Injectable({
