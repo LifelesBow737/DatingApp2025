aa96a2e1ba624f5774afa43bd94c5406e7c4a134
LifelesBow737 <diegolm115935@outlook.com>
2026-03-04 19:42:25 -0600

    Parcial04 - Add loading interceptor

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,12 +1,37 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { InitService } from '../core/services/init-service';
+import { lastValueFrom } from 'rxjs';
+import { errorInterceptor } from '../core/interceptors/error-interceptor';
+import { jwtInterceptor } from '../core/interceptors/jwt-interceptor';
+import { loadingInterceptor } from '../core/interceptors/loading-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideZonelessChangeDetection(),
+    provideRouter(routes, withViewTransitions()),
+    provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor, loadingInterceptor])),
+    provideAppInitializer(async () => {
+      const initService = inject(InitService);
+
+      return new Promise<void>((resolve) => {
+        setTimeout(async () => {
+          try {
+            return lastValueFrom(initService.init())
+          } finally {
+            const splash = document.getElementById("initial-splash");
+
+            if (splash) {
+              splash.remove();
+            }
+
+            resolve();
+          }
+        }, 500);
+      });
+    })
   ]
 };
